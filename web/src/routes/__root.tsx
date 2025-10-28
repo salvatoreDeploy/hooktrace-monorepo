33c4237ebd6140df66da8a28f55a1b6892b92c28
Henrique J. Araujo <liderhenrique@gmail.com>
2025-10-28 19:38:07 -0300

    feat: Configuração base para o front-end

---
+++ web/src/routes/__root.tsx
@@ -0,0 +1,15 @@
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+
+const RootLayout = () => (
+  <>
+    <div className="p-2 flex gap-2">
+      <Link to="/" className="[&.active]:font-bold">
+        Home
+      </Link>{' '}
+    </div>
+    <hr />
+    <Outlet />
+  </>
+)
+
+export const Route = createRootRoute({ component: RootLayout })
